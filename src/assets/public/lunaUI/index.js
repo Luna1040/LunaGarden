72c0f7eb86137229a8248254a996f0f992277b1d
liang@bjrrtx.com <Luna1040>
2020-01-13 22:25:04 +0800

    feat: 组件化开发

---
+++ src/assets/public/lunaUI/index.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import Modal from './modal.vue'
+import Button from './button.vue'
+import Input from './input.vue'
+
+Vue.component([{
+    Modal: 'Modal'
+}]);
---
+++ src/assets/public/lunaUI/modal.vue
@@ -0,0 +1,208 @@
+<template>
+  <div class="lunaModal" :class="{zIndexHide: !showModal}" :style="wrapStyles">
+    <transition name="maskAnimation">
+      <div class="modalMask" @click="handleMask()" v-show="showModal" v-if="showMask">
+      </div>
+    </transition>
+    <transition name="modalAnimation">
+      <div class="modalContainer" :style="[styles, modalWidth]" :class="className" v-show="showModal">
+        <div class="modalHeader" v-if="showHead">
+          <slot name="header">
+            <p>{{title}}</p>
+          </slot>
+          <i class="iconfont icon-cancel" @click="close"></i>
+        </div>
+        <div class="modalBody" :style="[modalHeight, modalMaxHeight, modalScrollable]">
+          <slot></slot>
+        </div>
+        <div class="modalFooter" v-if="footerShow">
+          <slot name="footer">
+            <button class="normalButton" v-waves @click="close">{{$t('lang.home.button.cancel')}}</button>
+            <button class="primaryButton" v-waves @click="close">{{$t('lang.home.button.confirm')}}</button>
+          </slot>
+        </div>
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      //弹窗的开关
+      value: {
+        type: Boolean,
+        default: false
+      },
+      //是否显示遮罩层
+      showMask: {
+        type: Boolean,
+        default: true
+      },
+      //是否允许遮罩层关闭
+      maskClosable: {
+        type: Boolean,
+        default: true
+      },
+      //title文字
+      title: {
+        type: String,
+      },
+      //是否显示右上角关闭按钮图标  关闭后 Esc 按键也将屏蔽
+      closeable: {
+        type: Boolean,
+        default: true
+      },
+      //自定义类名
+      className: {
+        type: String
+      },
+      //自定义样式
+      styles: {
+        type: Object,
+        default() {
+          return {};
+        }
+      },
+      //自定义z-index
+      zIndex: {
+        type: Number,
+        default: 250
+      },
+      //是否显示底部栏
+      footerShow: {
+        type: Boolean,
+        default: true
+      },
+      //是否可以滚动body区域
+      scrollable: {
+        type: Boolean,
+        default: false
+      },
+      //自定义宽度  作用于container
+      width: {
+        type: [Number, String],
+        default: 600,
+      },
+      //自定义高度  作用于body
+      height: {
+        type: [Number, String],
+        default: 'auto',
+      },
+      //自定义最大高度  作用于body
+      maxHeight: {
+        type: [Number, String],
+        default: 'auto',
+      },
+    },
+    data() {
+      return {
+        showModal: this.value,
+        showHead: true
+      }
+    },
+    methods: {
+      //点击右上角和默认关闭按钮时触发函数  外部调用@on-cancel()
+      close () {
+        this.showModal = false;
+        this.$emit('on-cancel');
+      },
+      //点击遮罩层触发事件
+      handleMask () {
+        if (this.closeable && this.showMask) {
+          this.close();
+        }
+      },
+      // 默认的底部按钮取消事件
+      cancel () {
+        this.close();
+      },
+    },
+    mounted() {
+      let showHead = true;
+      if (this.$slots.header === undefined && !this.title) {
+        showHead = false;
+      }
+      this.showHead = showHead;
+    },
+    computed: {
+      wrapStyles () {
+        return {
+          zIndex: this.zIndex
+        };
+      },
+      modalWidth () {
+          if(this.width !== 'auto') {
+            return {
+              width: this.width + 'px'
+            }
+        } else {
+            return {
+              width: this.width
+            }
+          }
+      },
+      modalHeight() {
+        if(this.height !== 'auto') {
+          return {
+            height: this.height + 'px'
+          }
+        } else {
+          return {
+            height: this.height
+          }
+        }
+      },
+      modalMaxHeight() {
+        if(this.maxHeight !== 'auto') {
+          return {
+            maxHeight: this.maxHeight + 'px'
+          }
+        } else {
+          return {
+            maxHeight: this.maxHeight
+          }
+        }
+      },
+      modalScrollable() {
+        if(this.scrollable) {
+          return {
+            overflow: 'auto',
+          }
+        } else {
+          return {
+            overflow: 'visible',
+          }
+        }
+      }
+    },
+    watch: {
+      value (val) {
+        this.showModal = val
+      },
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .maskAnimation-enter-active, .maskAnimation-leave-active {
+    transition: opacity 0.5s;
+  }
+  .maskAnimation-enter, .maskAnimation-leave-to {
+    opacity: 0;
+  }
+  .maskAnimation-leave,.maskAnimation-enter-to{
+    opacity: 1;
+  }
+  .modalAnimation-enter-active,.modalAnimation-leave-active{
+    transition: all 0.3s;
+  }
+  .modalAnimation-leave,.modalAnimation-enter-to{
+    opacity: 1;
+    top: 0;
+  }
+  .modalAnimation-enter, .modalAnimation-leave-to {
+    opacity: 0;
+    top: -24px;
+  }
+</style>
