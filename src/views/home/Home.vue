691f73359a1d73b5d21da2e27e8240e7f9bf940d
Luna Lovegood <liang@bjrrtx.com>
2020-01-25 10:38:53 +0800

    feat: 更新寻找层级公用部件、Col、Row、Badge组件更新

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -24,17 +24,13 @@
       </button>
     </div>
     <Row background="red">
-      <Col :span="8" background="lightblue">test</Col>
-      <Col :span="8" background="lightblue">test</Col>
+      <Col :span="8" background="lightblue" border="1px solid blue">test</Col>
       <Col :span="4" background="yellow">test</Col>
       <Col :span="2" background="skyblue">test</Col>
       <Col :span="1" background="pink">test</Col>
       <Col :span="3" background="hotPink">test</Col>
       <Col :span="3" background="#4a4a4a" color="#FFFFFF">test</Col>
       <Col :span="2" background="purple" color="#FFFFFF">test</Col>
-      <Col :span="3" background="#4a4a4a" color="#FFFFFF">test</Col>
-      <Col :span="3" background="#4a4a4a" color="#FFFFFF">test</Col>
-      <Col :span="8" background="lightblue">test</Col>
     </Row>
     <ul class="searchAssociation">
       <li
