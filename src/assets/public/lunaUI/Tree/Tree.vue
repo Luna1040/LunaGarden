fef0d49ed7844574b82011e7fe4ec7e2ea4349a1
Luna <z13021971276@outlook.com>
2020-04-25 16:00:11 +0800

    树状图bug修复

--- src/assets/public/lunaUI/Tree/Tree.vue
+++ src/assets/public/lunaUI/Tree/Tree.vue
@@ -1,20 +1,21 @@
 <template>
   <div class="lunaTree">
     <div class="lunaTreeTitle">
-      <img v-if="titleImg !== ''" :src="titleImg" alt="" /><i
+      <img v-if="titleImg !== ''" :src="titleImg" alt="" />
+      <i
         v-if="titleIcon !== ''"
         :class="titleIcon"
       ></i
       >{{ title }}
     </div>
-    <p v-if="this.data.length === 0">{{ emptyText }}</p>
+    <p v-if="data.length === 0">{{ emptyText }}</p>
     <div v-else>
       <ul>
         <TreeItem
           v-for="(i, index) in stateData"
           :data="i"
           :index="index"
-          :listName="i.title"
+          :list-name="i.title"
         ></TreeItem>
       </ul>
     </div>
@@ -22,46 +23,46 @@
 </template>
 
 <script>
-import TreeItem from "./TreeItem.vue";
+import TreeItem from './TreeItem.vue'
 
 export default {
-  name: "Tree",
+  name: 'Tree',
   components: {
-    TreeItem,
+    TreeItem
   },
   props: {
     data: {
       type: Array,
-      default: [],
+      default: []
     },
     title: {
       type: String,
-      default: "",
+      default: ''
     },
     titleIcon: {
       type: String,
-      deafult: "",
+      default: ''
     },
     titleImg: {
       type: String,
-      default: "",
+      default: ''
     },
     emptyText: {
       type: String,
-      default: "暂无数据",
+      default: '暂无数据'
     },
     showCheckbox: {
       type: Boolean,
-      deafault: false,
-    },
+      deafault: false
+    }
   },
-  data() {
+  data () {
     return {
-      stateData: this.data,
-    };
+      stateData: this.data
+    }
   },
-  methods: {},
-};
+  methods: {}
+}
 </script>
 
 <style scoped></style>
