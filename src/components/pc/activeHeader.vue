fdece52ad9e99dbe5f75b012ddc5a75d5379ce13
Luna <z13021971276@outlook.com>
2020-09-25 14:15:02 +0800

    LunaGarden Header重构

--- src/components/pc/activeHeader.vue
+++ src/components/pc/activeHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <!--  H5常见标签header-->
-  <header class="header" :class="{collapse: isCollapse}">
+  <header class="header" :class="{ collapse: isCollapse }">
     <!--    折叠标签后  共四个div分组-->
     <!--    用户区域-->
     <div class="userArea">
@@ -18,20 +18,19 @@
     </div>
     <!--    H5常见标签nav  通常用作header下方的-->
     <transition name="modalAnimation">
-    <nav v-show="!isCollapse" class="menuGroup">
+      <nav v-show="!isCollapse" class="menuGroup">
+        <!--      H5常见标签figure  通常结构为figure>img>figcaption，标准结构为上图下文字，在此处我使用i标签的字体图标代替了img标签-->
+        <!--      这里利用v-for循环生成了菜单列表，:key即是v-bind:key的简写，语法糖，v-for必须声明每个遍历生成节点的key值才可正常进行一些工作，所以无论如何必须同时使用-->
+        <!--        这里的:class是v-bind:class的缩写，语法糖。:class可以利用函数判断进行自动赋值，这里的函数意思为：当activeID等于这个menuItem的id时，为它增加一个叫做menuItemActive的class类名-->
+        <figure v-for="i in menuList" :key="i.id" class="menuItem" :class="{ menuItemActive: activeID === i.id }" @click="choiceMenu(i.id, i.link)">
+          <!--        这里的:class并没有书写函数表达式，意思为遍历数组内容时，用icon属性填充至class内-->
+          <i class="iconfont" :class="i.icon"></i>
+          <!--        -->
+          <!--        这里是i-18n的表达式，通常是{{ $t('lang.menus.xx') }}这样书写，以调用对象下的值，但我们需要动态赋值xx时，可以利用[xx]进行赋值，与上述的写法是一样的效果-->
+          <figcaption>{{ $t('lang.menus')[i.title] }}</figcaption>
+        </figure>
+      </nav>
     </transition>
-      <!--      H5常见标签figure  通常结构为figure>img>figcaption，标准结构为上图下文字，在此处我使用i标签的字体图标代替了img标签-->
-      <!--      这里利用v-for循环生成了菜单列表，:key即是v-bind:key的简写，语法糖，v-for必须声明每个遍历生成节点的key值才可正常进行一些工作，所以无论如何必须同时使用-->
-      <!--        这里的:class是v-bind:class的缩写，语法糖。:class可以利用函数判断进行自动赋值，这里的函数意思为：当activeID等于这个menuItem的id时，为它增加一个叫做menuItemActive的class类名-->
-      <figure v-for="i in menuList" :key="i.id" class="menuItem" :class="{menuItemActive: activeID === i.id}"
-              @click="choiceMenu(i.id, i.link)">
-        <!--        这里的:class并没有书写函数表达式，意思为遍历数组内容时，用icon属性填充至class内-->
-        <i class="iconfont" :class="i.icon"></i>
-        <!--        -->
-        <!--        这里是i-18n的表达式，通常是{{ $t('lang.menus.xx') }}这样书写，以调用对象下的值，但我们需要动态赋值xx时，可以利用[xx]进行赋值，与上述的写法是一样的效果-->
-        <figcaption>{{ $t('lang.menus')[i.title] }}</figcaption>
-      </figure>
-    </nav>
     <div class="guide">
       <div class="btnGroup">
         <!--        这里的:class表达式，意为：当全局变量$i18n.locale不为'zh-CN'这个字符串时，为这个英文的图标添加名为languageActive的class，实现选中的高亮效果(注：因为目前只有中英双语，那么只能用中文做判断)-->
@@ -44,8 +43,7 @@
         </div>
       </div>
       <!--      这里是LunaUI中的Button组件，其中的属性均是自定义的属性，需要注意的是，当你需要往属性中填充数字类型，布尔类型值，或者函数的时候，需要在属性前加入:，即v-bind:属性名-->
-      <Button color="#eb7290" icon="iconfont icon-guide" :icon-size="22" pre :width="130" :height="44" corner="full"
-              @click="toAbout">
+      <Button color="#eb7290" icon="iconfont icon-guide" :icon-size="22" pre :width="130" :height="44" corner="full" @click="toAbout">
         <!--        由于LunaUI的Button组件内使用了插槽，所以可以在这个标签内包含自定义的信息，会将预设的组件内容改为自定义的内容-->
         <span>{{ $t('lang.titles.Guide') }}</span>
       </Button>
@@ -97,7 +95,7 @@ export default {
       imgSrc: '',
       langSlideDown: '',
       // 不加引号就是数字类型
-      activeID: 1,
+      activeID: 1
     }
   },
   // 页面初始化完成后，立刻执行的函数，可以放在created内或mounted内，但mounted中一般存放钩子函数
@@ -133,8 +131,7 @@ export default {
   },
   // 这里存放了在该组件中所有自定义的函数
   methods: {
-    showSettings() {
-    },
+    showSettings() {},
     // 设置语言，括号内为接收到的参数
     setLang(value) {
       this.$i18n.locale = value
